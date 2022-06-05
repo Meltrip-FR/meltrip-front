c32038fba534c5ca6f3ece8fa6f0fb2d3adf26d4
Mathieu <drapalamathieu95@gmail.com>
2022-06-05 16:58:18 +0200

    feat: deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             window.dataLayer = window.dataLayer || [];
             function gtag() {
               dataLayer.push(arguments);
-            }
+            } 
             gtag('js', new Date());
             gtag('config', 'G-EBJFPBQ4GX');
         `}
