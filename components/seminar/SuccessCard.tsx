83df5d3998dfe8f0c71159c2c78219a7f5a07075
mDrapala <drapalamathieu95@gmail.com>
2022-12-26 12:54:17 +0100

    feat: add loader and redirect to success modal for create seminar

---
+++ components/seminar/SuccessCard.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import LogoColor from "../assets/icons/logoColor";
+import Love from "../assets/icons/love";
+
+const SuccessCard = () => {
+  return (
+    <div className="bg-base-100">
+      <div className="justify-center flex">
+        <LogoColor size={100} />
+      </div>
+      <p className="text-[#263238] text-justify px-12">
+        Nous avons toutes les infos dont nous avons besoins pour vous concocter
+        votre escapade.
+      </p>
+      <div className="justify-center flex">
+        <Love size={500} />
+      </div>
+
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-[#CE5729]">À bientôt !</h2>
+        <p className="card-title text-[#263238]">L’équipe Meltrip</p>
+        <div className="card-actions mt-5">
+          <Link
+            href="/user/seminar/list"
+            className="p-2 rounded text-white bg-meltrip-primary hover:bg-meltrip-ternary"
+          >
+            Mes séminaires
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessCard;
--- components/seminar/index.tsx
+++ components/seminar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 import FirstSignup from "@/components/seminar/FirstSignup";
 import SecondSignup from "@/components/seminar/SecondSignup";
@@ -9,6 +9,7 @@ import PresentSeminar from "./PresentSeminar";
 import { useRouter } from "next/router";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { login } from "@/redux/slices/auth.slice";
+import SuccessCard from "./SuccessCard";
 
 const SignupPage = () => {
   const router = useRouter();
@@ -16,8 +17,14 @@ const SignupPage = () => {
   const { auth } = useAppSelector((state) => state);
 
   const [pathname, setPathName] = useState<string>("");
-  const [nextPage, setNextPage] = useState(0);
-  const [errorMessage, setErrorMessage] = useState("Error: ");
+  const [nextPage, setNextPage] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState({
+    status: false,
+    url: "",
+    message: "",
+  });
   const [formState, setFormState] = useState({
     participNumber: 0,
     knowDate: true,
@@ -53,6 +60,7 @@ const SignupPage = () => {
   };
 
   const handSubmit = async () => {
+    setLoading(true);
     let addOrganizations: any;
     let addUsers: any;
     let loginRequestUser: any;
@@ -63,8 +71,13 @@ const SignupPage = () => {
         .post(`${process.env.NEXT_PUBLIC_API_URL}/organization/`, {
           siret: formState.siretCompany,
         })
-        .catch((_e) => {
-          alert("Siret introuvable veillez le saisir de nouveau !");
+        .catch((e) => {
+          console.error(e);
+          setErrorMessage({
+            status: true,
+            url: "/",
+            message: "Erreur lors de la création de votre entreprise",
+          });
         });
 
       if (addOrganizations.data) {
@@ -80,8 +93,13 @@ const SignupPage = () => {
             idOrganization: addOrganizations?.data?.id,
             roles: ["user"],
           })
-          .catch((_e) => {
-            alert("Error lors de la création de votre compte");
+          .catch((e) => {
+            console.error(e);
+            setErrorMessage({
+              status: true,
+              url: "/",
+              message: "Erreur lors de la création de votre compte",
+            });
           });
         //Login user
         loginRequestUser = await axios
@@ -114,8 +132,13 @@ const SignupPage = () => {
               return data;
             }
           })
-          .catch((_e) => {
-            alert("Error lors de la création séminaire");
+          .catch((e) => {
+            console.error(e);
+            setErrorMessage({
+              status: true,
+              url: "/",
+              message: "Erreur lors de la création de votre compte",
+            });
           });
       }
     }
@@ -147,6 +170,8 @@ const SignupPage = () => {
           accompaniedSuggest: formState.accompaniedSuggest,
           financialEmail: formState.emailFinancial,
           financialPhone: formState.numberFinancial,
+          status: "Attente",
+          step: "",
           idUser: loginUser?.id ? loginUser?.id : auth?.user?.id,
           idPayement: null,
           idQuote: null,
@@ -157,7 +182,14 @@ const SignupPage = () => {
           },
         }
       )
-      .catch((e) => console.error(e));
+      .catch((e) => {
+        console.error(e);
+        setErrorMessage({
+          status: true,
+          url: "/",
+          message: "Erreur lors de la création de votre séminaire",
+        });
+      });
 
     const createGroup: any = await axios
       .post(`${process.env.NEXT_PUBLIC_API_URL}/group/`, {
@@ -169,10 +201,22 @@ const SignupPage = () => {
         present: false,
         resultTest: 0,
       })
-      .catch((e) => console.error(e));
+      .catch((e) => {
+        console.error(e);
+        setErrorMessage({
+          status: true,
+          url: "/",
+          message: "Erreur lors de la création de votre groupe",
+        });
+      });
 
     if (addSeminar.data && createGroup.data) {
-      router.push("/user/seminar");
+      setLoading(false);
+      setOpenModal(true);
+      setTimeout(() => {
+        console.log("timeout");
+        //router.push("/user/seminar");
+      }, 3000);
     }
   };
 
@@ -180,55 +224,99 @@ const SignupPage = () => {
     setPathName(router.pathname);
   }, [router.pathname]);
 
-  return (
-    <div className="w-full bg-gray-100">
-      <div className="relative">
-        <img
-          className="object-none w-full h-64"
-          src="https://www.naturepaysage.photo/wp-content/uploads/Nature-Paysage-portfolio-17-novembre-2019-0017-3.jpg"
-          alt="picture"
-        />
-        <div className="absolute top-3 ml-[35%] mt-[5%] font-poppins">
-          <h1 className="text-white text-5xl">CRÉONS VOTRE SÉMINAIRE</h1>
-        </div>
-      </div>
-      <div className="mx-auto max-w-7xl w-[75%] sm:px-6 lg:px-8">
-        <div className=" bg-white px-24 py-10">
-          {nextPage === 0 ? (
-            <PresentSeminar setNextPage={setNextPage} />
-          ) : nextPage === 1 ? (
-            <FirstSignup
-              formState={formState}
-              setFormState={setFormState}
-              setNextPage={setNextPage}
-            />
-          ) : nextPage === 2 ? (
-            <SecondSignup
-              formState={formState}
-              onFormChange={onFormChange}
-              setFormState={setFormState}
-              setNextPage={setNextPage}
-            />
-          ) : nextPage === 3 ? (
-            <ThreeSignup
-              formState={formState}
-              onFormChange={onFormChange}
-              setFormState={setFormState}
-              handSubmit={handSubmit}
-              setNextPage={setNextPage}
-            />
-          ) : (
-            <ThreePointOneSignup
-              formState={formState}
-              onFormChange={onFormChange}
-              setFormState={setFormState}
-              handSubmit={handSubmit}
-              setNextPage={setNextPage}
-            />
-          )}
-        </div>
-      </div>
+  return openModal ? (
+    <div className="w-full  flex justify-center items-center bg-gray-100">
+      <SuccessCard />
     </div>
+  ) : (
+    <Fragment>
+      <div className="w-full bg-gray-100">
+        <Fragment>
+          {loading && (
+            <Fragment>
+              <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+                {errorMessage.status ? (
+                  <p className="text-center text-white text-xl font-semibold">
+                    {errorMessage.message}
+                    <div
+                      onClick={() => {
+                        setLoading(false);
+                        setErrorMessage({
+                          status: false,
+                          url: "",
+                          message: "",
+                        });
+                      }}
+                      className="underline cursor-pointer"
+                    >
+                      Revenir
+                    </div>
+                  </p>
+                ) : (
+                  <Fragment>
+                    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+                    <h2 className="text-center text-white text-xl font-semibold">
+                      Envoie de votre séminaire...
+                    </h2>
+                    <p className="w-1/3 text-center text-white">
+                      Cela peut prendre quelques secondes, ne fermez pas cette
+                      page.
+                    </p>
+                  </Fragment>
+                )}
+              </div>
+            </Fragment>
+          )}
+          <div className="relative">
+            <img
+              className="object-none w-full h-64"
+              src="https://www.naturepaysage.photo/wp-content/uploads/Nature-Paysage-portfolio-17-novembre-2019-0017-3.jpg"
+              alt="picture"
+            />
+            <div className="absolute top-3 ml-[35%] mt-[5%] font-poppins">
+              <h1 className="text-white text-5xl">CRÉONS VOTRE SÉMINAIRE</h1>
+            </div>
+          </div>
+          <div className="mx-auto max-w-7xl w-[75%] sm:px-6 lg:px-8">
+            <div className=" bg-white px-24 py-10">
+              {nextPage === 0 ? (
+                <PresentSeminar setNextPage={setNextPage} />
+              ) : nextPage === 1 ? (
+                <FirstSignup
+                  formState={formState}
+                  setFormState={setFormState}
+                  setNextPage={setNextPage}
+                />
+              ) : nextPage === 2 ? (
+                <SecondSignup
+                  formState={formState}
+                  onFormChange={onFormChange}
+                  setFormState={setFormState}
+                  setNextPage={setNextPage}
+                />
+              ) : nextPage === 3 ? (
+                <ThreeSignup
+                  formState={formState}
+                  onFormChange={onFormChange}
+                  setFormState={setFormState}
+                  handSubmit={handSubmit}
+                  setNextPage={setNextPage}
+                />
+              ) : (
+                <ThreePointOneSignup
+                  formState={formState}
+                  onFormChange={onFormChange}
+                  setFormState={setFormState}
+                  handSubmit={handSubmit}
+                  setNextPage={setNextPage}
+                />
+              )}
+            </div>
+          </div>
+          )
+        </Fragment>
+      </div>
+    </Fragment>
   );
 };
 
