bb22e0cfd9c9bf5ce8ce3edf23b5736dddf3a3f9
mDrapala <drapalamathieu95@gmail.com>
2022-12-26 03:23:29 +0100

    fix lower case

--- components/user/profil/index.tsx
+++ components/user/profil/index.tsx
@@ -8,7 +8,7 @@ import axios from "axios";
 
 const capitalize = (str: string) => {
   return str
-    .toLowerCase()
+    ?.toLowerCase()
     .split(" ")
     .map((word) => {
       return word?.charAt(0).toUpperCase() + word?.slice(1);
