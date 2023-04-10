5c43facab19d7357b2ff8e1aab31db8035b914d4
mDrapala <drapalamathieu95@gmail.com>
2023-04-10 21:29:14 +0200

    cgv

---
+++ components/cgv/index.tsx
@@ -0,0 +1,179 @@
+const CGV = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">
+        Condition générale de vente
+      </h1>
+      <h6 className="font-bold text-[#757575] mt-5 ">
+        En vigueur au 01/05/2022
+      </h6>
+      <div className="container mx-auto mt-10">
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 1 : Objet et champ d{"'"}application
+        </h3>
+        <p className="mb-5">
+          Les présentes conditions générales de vente (CGV) constituent le socle
+          de la négociation commerciale et sont systématiquement adressées ou
+          remises à chaque acheteur pour lui permettre de passer commande. Les
+          conditions générales de vente décrites ci-après détaillent les droits
+          et obligations de la société MELTRIP et de son client dans le cadre de
+          la vente des marchandises suivantes : Organisation de séminaire selon
+          le besoin d’une entreprise (séminaire d’intégration, de management, de
+          team building, de commerce et de formation) et selon la personnalité
+          de tous les collaborateurs ; Proposition de séminaires prêts-à-l{"'"}
+          emploi non modifiable. Toute acceptation du devis/bon de commande, en
+          ce compris la clause « Je reconnais avoir pris connaissance et j{"'"}
+          accepte les conditions générales de vente ci-annexées » implique l
+          {"'"}adhésion sans réserve de l{"'"}acheteur aux présentes conditions
+          générales de vente.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">ARTICLE 2 : prix</h3>
+        <p>
+          Les prix des services vendues sont ceux en vigueur au jour de la prise
+          de commande. Ils sont libellés en euros et calculés hors taxes. Par
+          voie de conséquence, ils seront majorés du taux de TVA et des frais de
+          logistiques applicables au jour de la commande. La société MELTRIP s
+          {"'"}accorde le droit de modifier ses tarifs à tout moment. Toutefois,
+          elle s{"'"}engage à facturer les marchandises commandées aux prix
+          indiqués lors de l{"'"}enregistrement de la commande.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          ARTICLE 3 : Rabais et ristournes
+        </h3>
+        <p>
+          Les tarifs proposés comprennent les rabais et ristournes que la
+          société MELTRIP serait amenée à octroyer compte tenu de ses résultats
+          ou de la prise en charge par l{"'"}acheteur de certaines prestations.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">ARTICLE 4 : escompte</h3>
+        <p className="">
+          Aucun escompte ne sera consenti en cas de paiement anticipé.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 5: Modalités de paiement
+        </h3>
+        <p>
+          Le règlement des commandes s{"'"}effectue par carte bancaire. Les
+          règlements seront effectués aux conditions suivantes : Pour les
+          séminaires personnalisés; Règlement d’un acompte qui équivaut à 75% du
+          prix du devis une fois la proposition acceptée Règlement des 15%
+          restant une fois le séminaire terminé Pour les séminaires prêt à
+          l’emploi Paiement à 60 jours suivant la date de facturation
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 6: Retard de paiement
+        </h3>
+        <p>
+          En cas de défaut de paiement total ou partiel des services livrées à l
+          {"'"}échéance, l{"'"}acheteur doit verser à la société MELTRIP une
+          pénalité de retard égale à trois fois le taux de l{"'"}intérêt légal.
+          Le taux de l{"'"}intérêt légal retenu est celui en vigueur au jour de
+          la livraison des marchandises. Cette pénalité est calculée sur le
+          montant TTC de la somme restant due, et court à compter de la date d
+          {"'"}échéance du prix sans qu{"'"}aucune mise en demeure préalable ne
+          soit nécessaire. En sus des indemnités de retard, toute somme, y
+          compris l’acompte, non payée à sa date d’exigibilité produira de plein
+          droit le paiement d’une indemnité forfaitaire de 40 euros due au titre
+          des frais de recouvrement. Articles 441-10 et D. 441-5 du code de
+          commerce.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 7: Clause résolutoire
+        </h3>
+        <p>
+          Si dans les quinze jours qui suivent la mise en oeuvre de la clause
+          {'"'}Retard de paiement {'"'}, l{"'"}acheteur ne s{"'"}est pas
+          acquitté des sommes restant dues, la vente sera résolue de plein droit
+          et pourra ouvrir droit à l{"'"}allocation de dommages et intérêts au
+          profit de la société MELTRIP.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">ARTICLE 8 : livraison</h3>
+        <p>
+          La livraison de tous les documents nécessaires à la réalisation du
+          séminaire est effectuée par mail à l’adresse du compte avec laquelle
+          la demande de séminaire a été réalisée. Le délai de livraison indiqué
+          lors de l{"'"}enregistrement de la commande n{"'"}est donné qu{"'"}à
+          titre indicatif et n{"'"}est aucunement garanti. Par voie de
+          conséquence, tout retard raisonnable dans la livraison des produits ne
+          pourra pas donner lieu au profit de l{"'"}acheteur à : l{"'"}
+          allocation de dommages et intérêts ; l{"'"}annulation de la commande.
+          En cas de manquement dans les documents, le Client devra formuler
+          toutes les réserves nécessaires sur le bon de commande à réception du
+          mail contenant desdits documents. Ces réserves devront être, en outre,
+          confirmées par écrit dans les cinq jours suivant la livraison, par
+          courrier recommandé AR adressé à la société ou par réponse au mail.
+        </p>
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 9: Force majeure
+        </h3>
+        <p className="mb-10">
+          La responsabilité de la société MELTRIP ne pourra pas être mise en
+          œuvre si la non-exécution ou le retard dans l{"'"}exécution de l{"'"}
+          une de ses obligations décrites dans les présentes conditions
+          générales de vente découle d{"'"}un cas de force majeure. À ce titre,
+          la force majeure s{"'"}entend de tout événement extérieur,
+          imprévisible et irrésistible au sens de l{"'"}article 1148 du Code
+          civil.
+        </p>{" "}
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 10: Tribunal compétent
+        </h3>
+        <p className="mb-10">
+          Tout litige relatif à l{"'"}interprétation et à l{"'"}exécution des
+          présentes conditions générales de vente est soumis au droit français.
+          À défaut de résolution amiable, le litige sera porté devant le
+          Tribunal de commerce de PARIS.
+        </p>{" "}
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 11: Droit à l’image
+        </h3>
+        <p className="mb-10">
+          La passation de la commande suppose l’acceptation par le Client de
+          l’utilisation de ses références dans le cadre de la communication
+          commerciale de MELTRIP.
+        </p>{" "}
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 12: Données personnelles
+        </h3>
+        <p className="mb-10">
+          Le Client est informé que, dans le cadre de la gestion des opérations
+          relatives à la relation contractuelle avec ses clients, des données à
+          caractère personnel sont traitées par MELTRIP en tant que responsable
+          de traitement. En conformité avec la Loi française Informatique et
+          Liberté ainsi qu’avec les règles applicables depuis le 25 mai 2018 en
+          application du Règlement Général européen sur la Protection des
+          Données, les données concernées sont conservées par MELTRIP pendant
+          toute la durée de la relation contractuelle augmentée des durées
+          légales de prescription. Le Client bénéficie, dans les conditions
+          prévues par la loi, d’un droit d’interrogation, d’accès, de
+          rectification, d’un droit à la portabilité, à la limitation du
+          traitement et d’opposition pour raisons légitimes, ainsi que du droit
+          de définir des directives relatives au sort de ses données après sa
+          mort. Ces derniers peuvent exercer les droits dont ils disposent en
+          s’adressant à la société MELTRIP à l’adresse mail suivante :
+          contact@meltrip.fr. Le Client peut, en cas de contestation, former une
+          réclamation auprès de la CNIL dont les coordonnées figurent à
+          l’adresse suivante : https://www.cnil.fr.
+        </p>{" "}
+        <h3 className="font-bold text-xl mt-5 mb-5">
+          Article 13 : Clause de propriété intellectuelle
+        </h3>
+        <p className="mb-10">
+          Toutes les images, photographies, illustrations et design ainsi que
+          les textes et commentaires reproduits sur le site https://meltrip.fr
+          sont protégés par le droit d’auteur, le droit des marques et le droit
+          à l’image et ce pour le monde entier. Ces contenus diffusés sur le
+          site https://meltrip.fr sont la propriété pleine et entière de la
+          société Meltrip. À ce titre, seule l’utilisation pour un usage privé
+          et sous respect des dispositions du code de la propriété
+          intellectuelle est autorisée. Toute autre utilisation est constitutive
+          de contrefaçon et sanctionnée au titre de la propriété intellectuelle.
+          La reproduction totale ou partielle du site https://meltrip.fr est
+          strictement interdite.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CGV;
---
+++ pages/cgv/index.tsx
@@ -0,0 +1,12 @@
+import CGV from "@/components/cgv";
+import Home from "pages";
+
+const cgvPage = () => {
+  return (
+    <Home>
+      <CGV />
+    </Home>
+  );
+};
+
+export default cgvPage;
