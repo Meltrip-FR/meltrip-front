48ecb93509b76bd758cd731eeab6aca02da04ae8
mDrapala <drapalamathieu95@gmail.com>
2023-04-27 23:09:07 +0200

    kelly fix

--- components/blog/index.tsx
+++ components/blog/index.tsx
@@ -25,14 +25,14 @@ const BlogPage = () => {
           <div className="p-4 md:w-1/3" key={item.id}>
             <div className="h-full overflow-hidden">
               <img
-                className="lg:h-48 md:h-36 w-full object-cover object-center"
+                className=" w-full object-cover object-center"
                 src={item?.pictureURL}
                 alt="blog"
               />
               <div className="p-6">
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                   CATEGORY
-                </h2>
+                </h2> */}
                 <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                   {item.title} {item.id}
                 </h1>
--- components/seminar/InviteCard.tsx
+++ components/seminar/InviteCard.tsx
@@ -19,45 +19,48 @@ const InviteCard = ({ seminar }: any) => {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col px-[13vh] w-full flex-wrap">
       <ToastContainer />
-      <p className="text-justify">
-        Merci pour ces informations, nos Meltrippers sont en train de vous
-        concocté le séminaire qui vous convient.
-      </p>
-      <br />
-      <p className="text-justify">
-        Vous recevrez un mail de confirmation de votre inscription
-      </p>
-      <h5 className="font-bold text-xl my-5">Test de personnalité</h5>
-      <p className="text-justify">
-        Afin de mieux personnaliser votre séminaire, voici{" "}
-        <b>un quiz de personnalité*</b> à envoyé aux participants du séminaire.
-        <br /> Les réponses permettront à nos Meltrippers d’analyser la
-        personnalité de vos collaborateurs, inclu vous-même si vous y
-        participez.
-        <br /> <br /> <span className="underline">À noter :</span> nous vous
-        soumettrons nos propositions de séminaires lorsque le nombre de
-        participants aura été atteint <b>65%</b>.
-      </p>
-      <div className="mt-16 relative cursor-pointer">
-        <div onClick={() => copiueClipboard()}>
-          <InviteLink width={700} />
+      <div className="flex flex-col justify-center">
+        <p className="text-justify">
+          Merci pour ces informations, nos Meltrippers sont en train de vous
+          concocté le séminaire qui vous convient.
+        </p>
+        <br />
+        <p className="text-justify">
+          Vous recevrez un mail de confirmation de votre inscription
+        </p>
+        <h5 className="font-bold text-xl my-5">Test de personnalité</h5>
+        <p className="text-justify">
+          Afin de mieux personnaliser votre séminaire, voici{" "}
+          <b>un quiz de personnalité*</b> à envoyé aux participants du
+          séminaire.
+          <br /> Les réponses permettront à nos Meltrippers d’analyser la
+          personnalité de vos collaborateurs, inclu vous-même si vous y
+          participez.
+          <br /> <br /> <span className="underline">À noter :</span> nous vous
+          soumettrons nos propositions de séminaires lorsque le nombre de
+          participants aura été atteint <b>65%</b>.
+        </p>
+        <div className="mt-16 relative cursor-pointer">
+          <div onClick={() => copiueClipboard()}>
+            <InviteLink width={700} />
+          </div>
+          <div className="absolute font-bold top-[5.5vh] left-[15vh]">
+            <p>{`https://meltrip.fr/invites/${seminar?.id}`}</p>
+          </div>
         </div>
-        <div className="absolute top-[5.5vh] left-[20vh]">
-          <p>{`https://meltrip.fr/invites/${seminar?.id}`}</p>
+        <p className="font-bold text-xl text-center mt-16">
+          *vous pouvez retrouvez le lien dans votre espace
+        </p>
+        <div className="flex justify-center w-full mt-16">
+          <a
+            href="https://meltrip.fr/user/dashboard"
+            className="px-6 py-2 text-center font-bold text-white rounded bg-meltrip-primary"
+          >
+            Allez dans mon espace
+          </a>
         </div>
-      </div>
-      <p className="font-bold text-xl text-center mt-16">
-        *vous pouvez retrouvez le lien dans votre espace
-      </p>
-      <div className="flex justify-center w-full mt-16">
-        <a
-          href="https://meltrip.fr/user/dashboard"
-          className="px-6 py-2 text-center text-white rounded bg-meltrip-primary"
-        >
-          Allez dans mon espace
-        </a>
       </div>
     </div>
   );
