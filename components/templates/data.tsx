9288c44e5a846f1a0e2349635940a254235679b5
MDrapala <drapalamathieu95@gmail.com>
2023-06-05 23:14:06 +0200

    feat: update link

--- components/templates/data.tsx
+++ components/templates/data.tsx
@@ -5,7 +5,7 @@ const wideCardData = [
     description:
       "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros",
     imageLink:
-      "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+      "https://images.unsplash.com/photo-1439853949127-fa647821eba0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
   },
   {
     duration: 6,
@@ -13,14 +13,14 @@ const wideCardData = [
     description:
       "Séminaire rando dans le Mercantour et la Vallée des merveilles",
     imageLink:
-      "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+      "https://images.unsplash.com/photo-1605554022845-04ee8c141fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80"
   },
   {
     duration: 5,
     price: 1268,
     description: "Séminaire activités dans le Verdon en groupe",
     imageLink:
-      "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+      "https://images.unsplash.com/photo-1654504467256-1039acf447d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGUlMjBWZXJkb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
   }
 ]
 
@@ -180,8 +180,7 @@ const meets = [
   },
   {
     id: 2,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+    title: "Séminaire rando dans le Mercantour et la Vallée des merveilles",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -296,8 +295,7 @@ const meets = [
   },
   {
     id: 3,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+    title: "Séminaire activités dans le Verdon en groupe",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -412,8 +410,7 @@ const meets = [
   },
   {
     id: 4,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+    title: "La route des lavandes en vélo électrique",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -448,7 +445,7 @@ const meets = [
         rank: 2,
         image:
           "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Randonnée découverte de la presqu'île de Giens",
+        name: "Séminaire charme et prestige en Charente",
         description:
           "Découverte de la presqu'île de Giens et marche entre criques et calanques. Sur ce petit bout de terre découpé par les vents, vous observez une géologie très particulière et une végétation riche en senteur. Vous empruntez le sentier côtier par le port du Niel, la plage de l'Arboussière, la pointe d'Escampobariou et le port de la Madrague.Point de vue magnifique sur Porquerolles La Merveilleuse",
         location: "Hyères, Giens",
@@ -529,7 +526,7 @@ const meets = [
   {
     id: 5,
     title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+      "Séminaire balades guidées entre nature et patrimoine en Loire Atlantique",
     cardImage:
       "https://images.unsplash.com/photo-1592769434316-7513d251d98a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
     coverImage:
@@ -682,7 +679,7 @@ const meets = [
   {
     id: 6,
     title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+      "Séminaire balades guidées entre nature et patrimoine en Loire Atlantique",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -798,7 +795,7 @@ const meets = [
   {
     id: 7,
     title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -913,8 +910,7 @@ const meets = [
   },
   {
     id: 8,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+    title: "Séminaire rando dans le Mercantour et la Vallée des merveilles",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -1029,8 +1025,7 @@ const meets = [
   },
   {
     id: 9,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+    title: "Séminaire activités dans le Verdon en groupe",
     cardImage:
       "https://images.unsplash.com/photo-1592769434316-7513d251d98a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
     coverImage:
@@ -1182,8 +1177,7 @@ const meets = [
   },
   {
     id: 10,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+    title: "La route des lavandes en vélo électrique",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -1415,7 +1409,7 @@ const meets = [
   {
     id: 12,
     title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
+      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros",
     cardImage:
       "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     coverImage:
@@ -1810,122 +1804,6 @@ const meets = [
     duration: 6,
     estimatePrice: 378,
     capacity: 7,
-    environment: "MOUNTAIN",
-    disponibility: "2023-03-13",
-    activities: [
-      {
-        rank: 1,
-        image:
-          "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Arrivée à la presqu'île de Giens",
-        description:
-          "Arrivée dans l'après midi à votre hôtel situé au calme de la presqu'île de Giens, point de départ de vos randonnées.Petite balade pour vous dégourdir les jambes en visitant la partie médiévale d’Hyères.Hébergement : hôtelRepas : Petit-déjeuner inclus",
-        location: "Hyères, Giens",
-        habitation: {
-          name: "Hotel de charme en Cote D'azur (Hyères)",
-          images: [
-            "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-            "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-            "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-            "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-          ]
-        }
-      },
-      {
-        rank: 2,
-        image:
-          "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Randonnée découverte de la presqu'île de Giens",
-        description:
-          "Découverte de la presqu'île de Giens et marche entre criques et calanques. Sur ce petit bout de terre découpé par les vents, vous observez une géologie très particulière et une végétation riche en senteur. Vous empruntez le sentier côtier par le port du Niel, la plage de l'Arboussière, la pointe d'Escampobariou et le port de la Madrague.Point de vue magnifique sur Porquerolles La Merveilleuse",
-        location: "Hyères, Giens",
-        habitation: " Hotel de charme en Cote D'azur (Hyères)",
-        details: [
-          { name: "Marche", description: "4h-5h" },
-          { name: "Dénivelé positif ", description: "350m" },
-          { name: "Dénivelé négatif", description: "350m" },
-          { name: "Hébergement", description: "hôtel" },
-          { name: "Repas", description: "Petit-déjeuner inclus" }
-        ]
-      },
-      {
-        rank: 3,
-        image:
-          "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Balade et visites de Porquerolles (ouest)",
-        description:
-          "À pied, vous rejoignez le port d'embarquement à la Tour Fondue pour rejoindre l'île de Porquerolles (25 minutes de traversées). Vous découvrez sur l'île, son village et ses plages de sable doré. Vous contemplez la Pointe du Langoustier qui sépare une plage de sable blanc et une autre de sable noir. De nombreux sentiers de randonnées s'offrent à vous. Possibilité de louer un vélo pour faire le tour de l'île. Un vrai plus !",
-        location: "Hyères, Giens",
-        habitation: " Hotel de charme en Cote D'azur (Hyères)",
-        details: [
-          { name: "Transport", description: "bateau" },
-          { name: "Marche", description: "4h-5h" },
-          { name: "Dénivelé positif ", description: "250m" },
-          { name: "Dénivelé négatif", description: "250m" },
-          { name: "Hébergement", description: "hôtel" },
-          { name: "Repas", description: "Petit-déjeuner inclus" }
-        ]
-      },
-      {
-        rank: 4,
-        image:
-          "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Balade et visites de Porquerolles (ouest)",
-        description:
-          "Depuis le port principal d'Hyères que vous rejoignez en bus, vous embarquez pour Port Cros (1h de traversée). Cette île, Parc National depuis 1963, offre un espace naturel protégé, véritable paradis écologique. Les différents itinéraires vous permettent de jouir de toute la variété des paysages de l'île. Si vous emmenez des palmes, un masque et un tuba, vous pouvez faire le sentier sous marin à la plage de La Palus.",
-        location: "Hyères, Giens",
-        habitation: " Hotel de charme en Cote D'azur (Hyères)",
-        details: [
-          { name: "Transport en bateau, bus", description: "2h30" },
-          { name: "Marche", description: "3h-5h" },
-          { name: "Dénivelé positif ", description: "250m" },
-          { name: "Dénivelé négatif", description: "250m" },
-          { name: "Hébergement", description: "hôtel" },
-          { name: "Repas", description: "Petit-déjeuner inclus" }
-        ]
-      },
-      {
-        rank: 5,
-        image:
-          "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Retour sur l'île de Porquerolles",
-        description:
-          "Parce qu'une journée ne suffit pas, retour sur l'île de Porquerolles pour une découverte plus approfondie de ses campagnes. Superbe sentier côtier jusqu'à la pointe de la Galère, le Cap des Mèdes et la plage Notre-Dame.",
-        location: "Porquerolles, Giens",
-        habitation: " Hotel de charme en Cote D'azur (Hyères)",
-        details: [
-          { name: "Marche", description: "4h-5h" },
-          { name: "Dénivelé positif ", description: "250m" },
-          { name: "Dénivelé négatif", description: "250m" },
-          { name: "Hébergement", description: "hôtel" },
-          { name: "Repas", description: "Petit-déjeuner inclus" }
-        ]
-      },
-      {
-        rank: 6,
-        image:
-          "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-        name: "Fin du séjour",
-        description:
-          "Après le petit déjeuner, vous pouvez encore profiter des îles, des sentiers côtiers... Repas : Petit-déjeuner inclus",
-        location: "Hyères, Giens",
-        details: [{ name: "Repas", description: "Petit-déjeuner inclus" }]
-      }
-    ]
-  },
-  {
-    id: 16,
-    title:
-      "Baignades et randonnées dans les îles d'Or : Giens, Porquerolles et Port-Cros ",
-    cardImage:
-      "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    coverImage:
-      "https://images.unsplash.com/photo-1660401023330-318f599edfa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-    description:
-      "Votre séminaire débute sur la presqu'île de Giens dans ses parties les plus belles et préservées par son inattendu sentier du littoral. Vous poursuivez votre périple sur les îles d'Or avec Porquerolles où vous passez deux jours à parcourir ses sentiers bordés d'eucalyptus, ses plages douces et harmonieusement découpées et son village mexicain, sa campagne paradis du randonneur... puis Port-Cros, la sauvage et la préservée grâce à son parc national vous invite à continuer la découverte. Fonds marins foisonnants et sa côte sont le refuge des espèces sauvages et protégées, espèces endémiques qui vous dévoileront leurs secrets et la raison même de la création du parc.",
-    duration: 6,
-    estimatePrice: 900,
-    capacity: 8,
     environment: "MOUNTAIN",
     disponibility: "2023-03-13",
     activities: [
